ba5f5424eca2abcada02316f946744bdb55717d9
mbernth <michael@monovoce.dk>
2015-11-12 18:08:20 +0100

    Function update and icons

---
+++ Genesis-Mono-Basics/js/responsive-menu.js
@@ -0,0 +1,40 @@
+jQuery(function( $ ){
+	
+	if( $( document ).scrollTop() > 0 ){
+		$( '.site-header' ).addClass( 'dark' );			
+	}
+
+	// Add opacity class to site header
+	$( document ).on('scroll', function(){
+
+		if ( $( document ).scrollTop() > 0 ){
+			$( '.site-header' ).addClass( 'dark' );			
+
+		} else {
+			$( '.site-header' ).removeClass( 'dark' );			
+		}
+
+	});
+
+	$("header .genesis-nav-menu, .nav-primary .genesis-nav-menu, .nav-secondary .genesis-nav-menu").addClass("responsive-menu").before('<div class="responsive-menu-icon"></div>');
+
+	$(".responsive-menu-icon").click(function(){
+		$(this).next("header .genesis-nav-menu, .nav-primary .genesis-nav-menu, .nav-secondary .genesis-nav-menu").slideToggle();
+	});
+
+	$(window).resize(function(){
+		if(window.innerWidth > 768) {
+			$("header .genesis-nav-menu, .nav-primary .genesis-nav-menu, .nav-secondary .genesis-nav-menu, nav .sub-menu").removeAttr("style");
+			$(".responsive-menu > .menu-item").removeClass("menu-open");
+		}
+	});
+
+	$(".responsive-menu > .menu-item").click(function(event){
+		if (event.target !== this)
+		return;
+			$(this).find(".sub-menu:first").slideToggle(function() {
+			$(this).parent().toggleClass("menu-open");
+		});
+	});
+
+});
